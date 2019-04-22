601b9e31dda54b5eb593f60f3faf2ed2f8220b50
kmietotwy <kmietotwy@gmail.com>
2019-04-22 11:26:20 -1000

    tests for adding two weights to one side

--- index.js
+++ index.js
@@ -7,10 +7,42 @@ function scaleBalance(strArr) {
   let right = currentScale[1];
   let addedWeights = [];
 
+  //checks if starting weights are equal
   if (left === right) {
     return addedWeights.toString();
   }
 
+  //add two weighs to one side
+  for (let a = 0; a < currentScale.length; a++) {
+    let oneSide = currentScale[a];
+
+    for (let b = 0; b < weights.length; b++) {
+      let weight1 = weights[b];
+
+      for (let c = 0; c < weights.length; c++) {
+        if (b !== c) {
+          let weight2 = weights[c];
+          let addTwo = oneSide + weight1 + weight2;
+
+          if (oneSide === left) {
+            if (addTwo === right) {
+              addedWeights.push(weight1);
+              addedWeights.push(weight2);
+              return addedWeights.sort().join(',');
+            }
+          } else {
+            if (addTwo === left) {
+              addedWeights.push(weight1);
+              addedWeights.push(weight2);
+              return addedWeights.sort().join(',');
+            }
+          }
+        }
+      }
+    }
+  }
+
+  //add weights to both sides
   for (let i = 0; i < weights.length; i++) {
     let leftCheck = left + weights[i];
 
--- test/spec.js
+++ test/spec.js
@@ -16,6 +16,14 @@ describe('Scale Balance', function(){
     scaleBalance(["[5, 9]", "[1, 2, 6, 7]"]).should.be.a('string')
   })
 
+  it('should be able to add two weights to one side - check left', function(){
+    scaleBalance(["[5, 9]", "[0, 1, 3, 6]"]).should.equal('1,3')
+  })
+
+  it('should be able to add two weights to one side - check right', function(){
+    scaleBalance(["[9, 5]", "[0, 1, 3, 6]"]).should.equal('1,3')
+  })
+
   it('should return a unique solution', function(){
     scaleBalance(["[5, 9]", "[1, 2, 6, 7]"]).should.equal('2,6')
   })
@@ -23,6 +31,5 @@ describe('Scale Balance', function(){
   it('should return not possible if no solution is found', function(){
     scaleBalance(["[5, 9]", "[0, 3, 6]"]).should.equal('not possible')
   })
-
 
 });
