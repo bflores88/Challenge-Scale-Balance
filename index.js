4b1b55aa7b7d6baa0400160228ad7f278b2a5542
kmietotwy <kmietotwy@gmail.com>
2019-04-22 10:19:25 -1000

    tests written

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-function scaleBalance(strArr) {
+'use strict';
 
+function scaleBalance(strArr) {
+  let currentScale = JSON.parse(strArr[0]);
+  let weights = JSON.parse(strArr[1]);
+  let left = currentScale[0];
+  let right = currentScale[1];
+  let addedWeights = [];
+
+  if (left === right) {
+    return addedWeights.toString();
+  }
+
+  for (let i = 0; i < weights.length; i++) {
+    let leftCheck = left + weights[i];
+
+    for (let j = 0; j < weights.length; j++) {
+      if (i !== j) {
+        let rightCheck = right + weights[j];
+
+        if (leftCheck === rightCheck) {
+          addedWeights.push(weights[i]);
+          addedWeights.push(weights[j]);
+        }
+      }
+    }
+  }
+
+  if (addedWeights.length === 0) {
+    return 'not possible';
+  } else {
+    return addedWeights.sort().join(',');
+  }
 }
 
 module.exports = scaleBalance;
--- test/spec.js
+++ test/spec.js
@@ -1,1 +1,28 @@
 // write tests here
+'use strict';
+
+const scaleBalance = require('../index.js');
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+
+describe('Scale Balance', function(){
+
+  it('should be a function', function(){
+    scaleBalance.should.be.a('function')
+  })
+
+  it('should return a string', function(){
+    scaleBalance(["[5, 9]", "[1, 2, 6, 7]"]).should.be.a('string')
+  })
+
+  it('should return a unique solution', function(){
+    scaleBalance(["[5, 9]", "[1, 2, 6, 7]"]).should.equal('2,6')
+  })
+
+  it('should return not possible if no solution is found', function(){
+    scaleBalance(["[5, 9]", "[0, 3, 6]"]).should.equal('not possible')
+  })
+
+
+});
